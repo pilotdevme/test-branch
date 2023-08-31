4994397dbbfca2d677f5edbc0270ad6a01f7a47c
ravermame <raverma.me@gmail.com>
2023-08-31 12:57:34 +0530

    naming conventions and variable data type fixes

--- src/app/popup/components/timer/timer.component.ts
+++ src/app/popup/components/timer/timer.component.ts
@@ -5,8 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { PopupService } from '../../../services/popup.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import moment from "moment";
-import { interval,Subscription } from 'rxjs';
-import { ISelectedValues, IList, ITime, ITimeDifference, ITimeEntry, ILocalData } from './timer.interface';
+import { interval, Subscription } from 'rxjs';
+import { ISelectedValues, IList, ITime, ITimeDifference, ITimeEntry, ILocalData, IProject } from './timer.interface';
 import { enumChangeList, enumList, enumTime, enumTimeDifference, initialTimerValue } from './timer.enum';
 import { ChromeStorageService } from 'src/app/services/chromeService.service';
 
@@ -30,18 +30,18 @@ export class TimerComponent implements OnInit {
     public timerRunning: boolean = false;
     public timer: string = initialTimerValue;
 
-    private interval_subscription !: Subscription;
+    private intervalSubscription !: Subscription;
     private timeDifference: ITimeDifference = enumTimeDifference;
     private getLocData: ILocalData = {};
     private time: ITime = enumTime;
 
 
-    constructor(private popup_service: PopupService, private chrome_service: ChromeStorageService) { }
+    constructor(private popupService: PopupService, private chrome_service: ChromeStorageService) { }
 
     /* get project details and timer */
     getTimers() {
         try {
-            this.popup_service.getTimeEntries().subscribe((response: any) => {
+            this.popupService.getTimeEntries().subscribe((response: any) => {
                 this.timeEntries = response;
             });
         }
@@ -50,7 +50,7 @@ export class TimerComponent implements OnInit {
 
     /* start time interval function */
     startTimer() {
-        let body = {
+        const body = {
             "isBillable": this.selectedValues.isBillable,
             "isBilled": false,
             "taskId": this.selectedValues.task,
@@ -59,19 +59,19 @@ export class TimerComponent implements OnInit {
             "typeOfWorkId": this.selectedValues.typeOfWork,
             "timezone": Intl.DateTimeFormat().resolvedOptions().timeZone
         };
-        this.popup_service.startTimeEntry(body).subscribe((response: any) => {
+        this.popupService.startTimeEntry(body).subscribe((response: any) => {
             this.timerRunning = true
             this.chrome_service.setStorageData({ running_time: this.timerRunning });
             this.chrome_service.setStorageData({ timer_start_time: response?.startTimeLocal });
             if (response) {
-                this.interval_subscription = interval(1000).subscribe(() => this.updateTimer());
+                this.intervalSubscription = interval(1000).subscribe(() => this.updateTimer());
             }
         });
     }
 
     /*stop time interval function*/
     stopTimer() {
-        this.popup_service.endTimeEntry().subscribe((response: any) => {
+        this.popupService.endTimeEntry().subscribe(() => {
             this.timerRunning = false;
             this.chrome_service.setStorageData({ running_time: this.timerRunning });
             this.chrome_service.setStorageData({ timer_start_time: ' ' });
@@ -80,7 +80,7 @@ export class TimerComponent implements OnInit {
             this.chrome_service.setStorageData({ select_task_value: ' ' });
 
             if (this.timerRunning === false) {
-                this.interval_subscription.unsubscribe();
+                this.intervalSubscription.unsubscribe();
                 this.timer = initialTimerValue;
                 this.time = enumTime;
                 this.selectedValues = enumChangeList;
@@ -91,79 +91,77 @@ export class TimerComponent implements OnInit {
 
     /* get work types */
     getWorkTypes() {
-        this.popup_service.getTypeOfWork().subscribe((response: any) => {
+        this.popupService.getTypeOfWork().subscribe((response: any) => {
             this.list.workTypes = response
         });
     }
 
     /* get projects list */
     getProjects() {
-        this.popup_service.getProjects().subscribe((response: any) => {
+        this.popupService.getProjects().subscribe((response: any) => {
             this.list.projects = response
         });
     }
 
     /* get task lists according to projects list */
     getTasks() {
-        this.popup_service.getTasks(this.selectedValues.project).subscribe((response: any) => {
+        this.popupService.getTasks(this.selectedValues.project).subscribe((response: any) => {
             this.list.tasks = response
         });
     }
 
     /* current time note */
-    noteChange(event: any) {
-        this.note = event.target.value
+    noteChange(event: Event) {
+        this.note = (event.target as HTMLInputElement)?.value
     }
 
     /* project list change data */
-    projectChange(event: any) {
-        this.selectedValues.project = event.target.value;
+    projectChange(event: Event) {
+        this.selectedValues.project = (event.target as HTMLInputElement)?.value;
         this.chrome_service.setStorageData({ select_project_value: this.selectedValues.project });
-        this.selectedValues.isBillable = this.list.projects?.find((project: any) => project.id == this.selectedValues.project)?.isBillableByDefault || false
+        this.selectedValues.isBillable = this.list.projects?.find((project: IProject) => project.id == this.selectedValues.project)?.isBillableByDefault || false
         this.getTasks()
     }
 
     /* task list change */
-    taskChange(event: any) {
-        this.selectedValues.task = event.target.value;
+    taskChange(event: Event) {
+        this.selectedValues.task = (event.target as HTMLInputElement)?.value;
         this.chrome_service.setStorageData({ select_task_value: this.selectedValues.task });
     }
 
     /* work type list change data */
-    typeOfWorkChange(event: any) {
-        this.selectedValues.typeOfWork = event.target.value;
+    typeOfWorkChange(event: Event) {
+        this.selectedValues.typeOfWork = (event.target as HTMLInputElement)?.value;
         this.chrome_service.setStorageData({ select_workType_value: this.selectedValues.typeOfWork });
     }
 
     /* get Date format for project tabel entry */
-    getDate(date: any) {
-        let formatted_date = moment(date).format("DD MMMM");
+    getDate(date: string) {
+        const formatted_date = moment(date).format("DD MMMM");
         return formatted_date;
     }
 
     /* get Time format for project tabel entry */
     getTime(time: string) {
-        let formatted_time = time?.substring(0, 5)
+        const formatted_time = time?.substring(0, 5)
         return formatted_time;
     }
 
     /* get start running interval time to end running interval time */
-    totalTrackingTime(time: any) {
+    totalTrackingTime(time: ITimeEntry) {
         /*Given time values as strings*/
-        const timer_start_time_interval = time?.startTimeLocal;
-        const end_time_interval = time?.endTimeLocal;
-
-        return this.timeDifferenceInterval(timer_start_time_interval, end_time_interval)
+        const { startTimeLocal, endTimeLocal } = time;
+        return this.timeDifferenceInterval(startTimeLocal, endTimeLocal)
     }
 
     /* calculate time difference between start running time interval to end running time interval */
-    timeDifferenceInterval(startTimeIntervalValue: any, endTimeIntervalValue: any) {
+    timeDifferenceInterval(startTimeIntervalValue: string, endTimeIntervalValue: string) {
 
         /*Convert time values to Date objects*/
-        let start_loc_time: any = new Date(`1970-01-01T${startTimeIntervalValue}Z`);
-        let end_loc_time: any = new Date(`1970-01-01T${endTimeIntervalValue}Z`);
+        const start_loc_time: any = new Date(`1970-01-01T${startTimeIntervalValue}Z`);
+        const end_loc_time: any = new Date(`1970-01-01T${endTimeIntervalValue}Z`);
 
-        let timeDifference = Math.abs(start_loc_time - end_loc_time);
+        const timeDifference = Math.abs(start_loc_time - end_loc_time);
 
         this.timeDifference.hour = Math.floor(timeDifference / 3600000); // 1 hour = 3600000 milliseconds
         this.timeDifference.minute = Math.floor((timeDifference % 3600000) / 60000); // 1 minute = 60000 milliseconds
@@ -183,8 +181,8 @@ export class TimerComponent implements OnInit {
         const milliseconds = currentDate.getMilliseconds();
 
         const formattedDateTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-        let timer_timer_start_time: number = this.getLocData?.timer_start_time || 0; // Replace with your start time
-        let timer_end_time: string = formattedDateTime; // Replace with your end time
+        const timer_timer_start_time = this.getLocData?.timer_start_time?.toString() || "0"; // Replace with your start time
+        const timer_end_time: string = formattedDateTime; // Replace with your end time
 
         return this.timeDifferenceInterval(timer_timer_start_time, timer_end_time);
     }
@@ -217,20 +215,17 @@ export class TimerComponent implements OnInit {
 
         /* selected project value */
         if (this.selectedValues.project == '') {
-            let stored_project_value = this.getLocData?.select_project_value ?? '';
-            this.selectedValues.project = stored_project_value;
+            this.selectedValues.project = this.getLocData?.select_project_value ?? '';
         }
 
         /* selected task value */
         if (this.selectedValues.task == '') {
-            let stored_task_value = this.getLocData?.select_task_value ?? '';
-            this.selectedValues.task = stored_task_value;
+            this.selectedValues.task = this.getLocData?.select_task_value ?? '';
         }
 
         /* selected work type value */
         if (this.selectedValues.typeOfWork == '') {
-            let stored_workType_value = this.getLocData?.select_workType_value ?? '';
-            this.selectedValues.typeOfWork = stored_workType_value;
+            this.selectedValues.typeOfWork = this.getLocData?.select_workType_value ?? '';
         }
 
         /* set current timer running time hours, minutes and seconds and call time interval again according to current running time */
@@ -240,7 +235,7 @@ export class TimerComponent implements OnInit {
                 this.time.hours = this.timeDifference.hour;
                 this.time.minutes = this.timeDifference.minute;
                 this.time.seconds = this.timeDifference.second;
-                this.interval_subscription = interval(1000).subscribe(() => this.updateTimer());
+                this.intervalSubscription = interval(1000).subscribe(() => this.updateTimer());
             }
         }
 
